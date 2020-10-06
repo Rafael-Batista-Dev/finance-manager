10055e1e2eca94ba2b38cf94906fb85842e9ecf3
unknown <rafaelbatista.sth@gmail.com>
2020-10-06 17:55:46 -0300

    delegation of Schemas with prism binding

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,30 @@
 const { GraphQLServer } = require("graphql-yoga");
+const Binding = require("prisma-binding");
 const { prisma } = require("./generated/prisma-client");
+
+const binding = new Binding.Prisma({
+  typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+  endpoint: process.env.PRISMA_ENDPOINT,
+});
 
 const resolvers = {
   Query: {
     user(parent, args, context, info) {
+      /*      
       return prisma.user({ id: args.id });
+      return {
+        id: "wfdsfasdsad",
+        name: "Rafael",
+        email: "dad@dasda.com",
+      };
+      */
+
+      return binding.query
+        .user({ where: { id: args.id } }, info)
+        .then((user) => {
+          console.log("User: ", user);
+          return user;
+        });
     },
   },
 };
